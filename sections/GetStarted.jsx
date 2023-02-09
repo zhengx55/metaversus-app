18b3d83add770bc988976b5d7c8488322a41412a
zhengx55 <williamzhxin@gmail.com>
2023-02-09 22:12:01 +0800

    hero about explore section

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,8 +1,21 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn, planetVariants, staggerContainer } from '../utils/motion';
+import { startingFeatures } from '../constants';
+import { TitleText, TypingText } from '../components';
+
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col gap-8 lg:flex-row`}>
+      <motion.div variants={planetVariants('left')}></motion.div>
+    </motion.div>
   </section>
 );
 
