a08bbb5f6cb793c446fb01f6a090b17f409c992a
zhengx55 <williamzhxin@gmail.com>
2023-02-10 11:06:05 +0800

    fully implemented

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,6 +1,14 @@
-const StartSteps = () => (
-  <div>
-    start steps
+import styles from '../styles';
+
+const StartSteps = ({ number, text }) => (
+  <div className={`${styles.flexCenter} flex-row`}>
+    <div
+      className={`${styles.flexCenter} h-[70px] w-[70px] rounded-[24px] bg-[#323f5d]`}>
+      <p className='text-[20px] font-bold text-white'>0{number}</p>
+    </div>
+    <p className='ml-[30px] flex-1 text-[18px] font-normal leading-[32px] text-[#B0B0B0]'>
+      {text}
+    </p>
   </div>
 );
 
--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn, planetVariants, staggerContainer } from '../utils/motion';
 import { startingFeatures } from '../constants';
-import { TitleText, TypingText } from '../components';
+import { StartSteps, TitleText, TypingText } from '../components';
 
 const GetStarted = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -14,7 +14,26 @@ const GetStarted = () => (
       whileInView='show'
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col gap-8 lg:flex-row`}>
-      <motion.div variants={planetVariants('left')}></motion.div>
+      <motion.div
+        variants={planetVariants('left')}
+        className={`${styles.flexCenter} flex-1`}>
+        <img
+          src='/get-started.png'
+          alt='get-started'
+          className='h-[90%] w-[90%] object-contain'
+        />
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className='flex flex-[0.75] flex-col justify-center'>
+        <TypingText title='| How Metaversus works' />
+        <TitleText title={<>Get started with just a few clicks</>} />
+        <div className='mt-[31px] flex max-w-[370px] flex-col gap-[24px]'>
+          {startingFeatures.map((item, index) => {
+            return <StartSteps key={item} number={index + 1} text={item} />;
+          })}
+        </div>
+      </motion.div>
     </motion.div>
   </section>
 );
